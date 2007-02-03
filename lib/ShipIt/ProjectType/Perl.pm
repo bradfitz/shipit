532799249e0b39a0bd3eb71be31e226f3c4836fd
Brad Fitzpatrick <brad@danga.com>
2007-02-03 23:04:43 +0000

    step 1:  perl version detection

--- lib/ShipIt/ProjectType/Perl.pm
+++ lib/ShipIt/ProjectType/Perl.pm
@@ -2,20 +2,46 @@ package ShipIt::ProjectType::Perl;
 use strict;
 use base 'ShipIt::ProjectType';
 
+# fields:
+#   version -- if defined, cached current version
+#   ver_from -- if Makefile.PL says so, what file our $VERSION comes from
+
 sub current_version {
+    my $self = shift;
+    return $self->{version} if defined $self->{version};
+
+    if (-e "Makefile.PL") {
+        return $self->{version} = $self->current_version_from_makefilepl;
+    } else {
+        die "TODO: don't yet support Module::Build, etc...\n";
+    }
+}
+
+sub current_version_from_makefilepl {
+    my $self = shift;
     open (my $fh, "Makefile.PL") or die "Can't open Makefile.PL: $!\n";
-    my $ver;
-    my $ver_from;
     while (<$fh>) {
         if (/VERSION_FROM.+([\'\"])(.+?)\1/) {
-            $ver_from = $2;
+            $self->{ver_from} = $2;
             last;
+        }
+        if (/VERSION.+([\'\"])(.+?)\1/) {
+            return $2;
         }
     }
     close($fh);
-    die "No VERSION_FROM or VERSION\n" unless $ver || $ver_from;
-    warn "ver_from=[$ver_from], ver=[$ver]\n";
+    return $self->version_from_file;
 }
 
+# returns $VERSION from a file, assuming $self->{ver_from} is already set
+sub version_from_file {
+    my $self = shift;
+    my $file = $self->{ver_from} or die "no ver_from set";
+    open (my $fh, $file) or die "Failed to open $file: $!\n";
+    while (<$fh>) {
+        return $2 if /\$VERSION\s*=\s*([\'\"])(.+?)\1/;
+    }
+    die "No \$VERSION found in file $file\n";
+}
 
 1;
