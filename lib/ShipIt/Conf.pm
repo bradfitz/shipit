975d2fd9b0c0fc4017580a1020a5b55ad90a9697
Brad Fitzpatrick <brad@danga.com>
2007-04-20 18:38:46 +0000

    docs

--- lib/ShipIt/Conf.pm
+++ lib/ShipIt/Conf.pm
@@ -1,6 +1,37 @@
 package ShipIt::Conf;
 use strict;
 
+=head1 NAME
+
+ShipIt::Conf -- holds/parses config info for a project
+
+=head1 SYNOPSIS
+
+ # done for you:
+ my $conf = ShipIt::Conf->parse(CONFFILE);
+
+ # fetch keys out of it in your Step's init method
+ package ShipIt::Step::Custom;
+ use base 'ShipIt::Step';
+ sub init {
+     my ($self, $conf) = @_;
+     ....
+     $self->{foo} = $conf->value("foo_key");
+     ....
+ }
+
+=cut
+
+=head1 CLASS METHODS
+
+=head2 parse
+
+  $conf = ShipIt::Conf->parse(".shipit");
+
+Returns a ShipIt::Conf object from a file.  Dies on parse failure.
+
+=cut
+
 sub parse {
     my ($class, $file) = @_;
     open (my $fh, $file) or die "Error opening config file $file: $!\n";
@@ -22,6 +53,14 @@ sub parse {
     return $self;
 }
 
+=head2 write_template
+
+    ShipIt::Conf->write_template($file);
+
+Writes out a dummy config file to the provided $file.
+
+=cut
+
 sub write_template {
     my ($class, $file) = @_;
     my $steps = $class->default_steps;
@@ -39,11 +78,28 @@ steps = $steps
     close($fh) or die;
 }
 
+=head1 INSTANCE METHODS
+
+=head2 value
+
+  $val = $conf->value($key);
+
+Fetch a config value.  (also marks it as a known key, so any unknown
+keys in a .shipit config file cause a configuration error)
+
+=cut
+
 sub value {
     my ($self, $key) = @_;
     $self->{asked}{$key} = 1;
     return $self->{val}{$key};
 }
+
+=head2 die_if_unknown_keys
+
+Die if any key exists which has never been asked for.
+
+=cut
 
 sub die_if_unknown_keys {
     my $self = shift;
@@ -64,6 +120,18 @@ sub default_steps {
                    MakeDist
                    ));
 }
+
+=head2 steps
+
+Returns array of ShipIt::Step instances, based on the value of
+B<steps> in your .shipit config file.  For instance, in your .shipit file:
+
+  steps = FindVersion, ChangeVersion, Commit, Tag, MakeDist
+
+The makes ShipIt::Step::FindVersion loaded & instantiated (with 'new', which
+calls by default 'init'), followed by ChangeVersion, etc.
+
+=cut
 
 # returns ShipIt::Step::Foo instances
 sub steps {
--- lib/ShipIt/State.pm
+++ lib/ShipIt/State.pm
@@ -3,6 +3,19 @@ use strict;
 use ShipIt::ProjectType::Perl;
 use ShipIt::VC;
 
+=head1 NAME
+
+ShipIt::State - holds state between steps
+
+=head1 OVERVIEW
+
+An instance of type ShipIt::State is passed to each L<ShipIt::Step>'s
+'run' method.
+
+=head1 INSTANCE METHODS
+
+=cut
+
 sub new {
     my ($class, $conf) = @_;
     my $self = bless {}, $class;
@@ -11,21 +24,58 @@ sub new {
     return $self;
 }
 
+=head2 set_version
+
+   $state->set_version("1.34");
+
+Set the version of this release.  (is done by
+L<ShipIt::Step::FindVersion>, early in the process, but perhaps you
+want to write your own version-detecting/selecting step)
+
+=cut
+
 sub set_version {
     my ($self, $ver) = @_;
     $self->{version} = $ver;
 }
+
+=head2 version
+
+   $ver = $state->version
+
+Returns version of release, or dies if not yet set.
+
+=cut
 
 sub version {
     my ($self, $ver) = @_;
     return $self->{version} or die "No version yet set";
 }
 
-# returns project-type instance (created on first access)
+=head2 pt
+
+    $pt = $state->pt;
+
+Detects/instantiates this project's type (Perl vs autoconf, etc) and
+returns a newly-created or previously-cached L<ShipIt::ProjectType>
+instance.
+
+=cut
+
 sub pt {
     my $self = shift;
     return $self->{pt} ||= ShipIt::ProjectType->new;
 }
+
+=head2 vc
+
+    $vc = $state->vc;
+
+Detects/instantiates the version control system in use (svn, svk, etc)
+and returns a newly-created or previously-cached L<ShipIt::VC>
+instance.
+
+=cut
 
 # returns a version-control-type instance (created on first access)
 sub vc {
@@ -33,18 +83,57 @@ sub vc {
     return $self->{vc};
 }
 
+=head2 add_changelog_file
+
+    $state->add_changelog_file("CHANGES");
+
+Push a changelog file into the known list.
+
+=cut
+
 sub add_changelog_file {
     my ($self, $file) = @_;
     push @{$self->{changelogs}}, $file;
 }
+
+=head2 add_changelog_file
+
+    @files = $state->changelog_files;
+
+Returns list of known changelog files.
+
+=cut
 
 sub changelog_files {
     my $self = shift;
     return @{$self->{changelogs}};
 }
 
+=head2 dry_run
+
+   $dry = $state->dry_run;
+
+Accessor for "dry run" flag.  If set, you shouldn't actually
+commit/tag/upload stuff... just test if everything would've succeeded.
+
+=cut
+
 sub set_dry_run { $_[0]{dryrun} = $_[1] }
 sub dry_run     { $_[0]{dryrun}         }
+
+=head2 set_distfile
+
+=head2 distfile
+
+   $state->set_distfile("foo.tar.gz");
+   $file = $state->distfile;
+
+Setter/getter for where the final distfile is.  Useful so steps
+that need to build the whole tree (say, in an early "make test"
+phase), can keep it around, and later steps (like "make dist") can
+access it, without recompiling everything again.
+
+=cut
 
 sub set_distfile { $_[0]{distfile} = $_[1] }
 sub distfile     { $_[0]{distfile}         }
